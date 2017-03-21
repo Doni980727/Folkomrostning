fe0d10748d0e5c82c5278bcecdb2a964a626ab62
xDoni <dinof98@gmail.com>
2017-03-21 15:08:35 +0100

    prutt

--- results.php
+++ results.php
@@ -1,31 +1,48 @@
+<!doctype html>
+<html lang="sv">
+
+<head>
+
+	<meta charset="utf-8">
+	<title>Folkomröstning</title>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="stylesheet.css">
+  <script src="script.js"></script>
+	<link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+
+</head>
+<body>
+
 <?php
 $servername = "localhost";
 $username = "root";
-$password = "password";
+$password = "";
 $dbname = "folkom";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+     die("Connection failed: " . $conn->connect_error);
 } 
 
-// sql to create table
-$sql = "CREATE TABLE namnin (
-id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
-firstname VARCHAR(30) NOT NULL,
-lastname VARCHAR(30) NOT NULL,
-city VARCHAR(30) NOT NULL,
-email VARCHAR(50),
-reg_date TIMESTAMP
-)";
+$sql = "SELECT id, firstname, lastname, city, email FROM namnin";
+$result = $conn->query($sql);
 
-if ($conn->query($sql) === TRUE) {
-    echo "Table namnin created successfully";
+if ($result->num_rows > 0) {
+     echo "<table class='table table-striped'><tr><th>ID</th><th>Name</th><th>City</th><th>Email</th></tr>";
+     // output data of each row
+     while($row = $result->fetch_assoc()) {
+         echo "<tr><td>" . $row["id"]. "</td><td>" . $row["firstname"]. " " . $row["lastname"]. "</td><td>" . $row["city"]. "</td><td>" . $row["email"]. "</td></tr>";
+     }
+     echo "</table>";
 } else {
-    echo "Error creating table: " . $conn->error;
+     echo "0 results";
 }
 
 $conn->close();
-?>
+?>  
+
+</body>
+</html>
--- script.js
+++ script.js
@@ -1,12 +1,6 @@
 $(document).ready(function(){
-    $("#in").click(function(){
+    $('#in').click(function(){
             $.post("action_page.php", $("form").serialize());
         
-    });
-});
-
-$("#table").click(function(){
-    $.get("results.php", function(data, status){
-        alert("Data: " + data + "\nStatus: " + status);
     });
 });
